aa741760e61de506bc1b313d6e4e4851f3a9cead
hdito <egorermolaef@gmail.com>
2021-12-13 23:34:50 +0300

    Add reading of player's input, turn result check and game simulation

--- script.js
+++ script.js
@@ -1,13 +1,57 @@
 function computerPlay(params) {
     const computerTurn = Math.floor(Math.random() * 3);
-    console.log(computerTurn)
     switch (computerTurn) {
         case 0:
-            return "Rock"
+            return "rock"
         case 1:
-            return "Paper"
+            return "paper"
         case 2:
-            return "Scissors"
+            return "scissors"
     }
 }
-computerPlay();
+
+function getPlayerTurn(params) {
+    const playerTurn = prompt("Your turn:", "") ?? "";
+    const standardizedTurnOutput = playerTurn.toLowerCase();
+    return standardizedTurnOutput;
+}
+
+function evaluateTurn(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return `It's a tie! ${playerSelection.slice(0, 1).toUpperCase() + playerSelection.slice(1)} can't beat another ${playerSelection}.`
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You lose! Paper beats rock"
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You win! Rock beats scissors"
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win! Paper beats rock"
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You lose! Scissors beat paper"
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You lose! Rock beats scissors"
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win! Scissors beat paper"
+    } else return "You lose! Show something!"
+
+}
+
+function game(rule, player, computer) {
+    let winnerCount = 0;
+    let loserCount = 0;
+    for (let index = 0; index < 5; index++) {
+        let result = rule(player(), computer());
+        if (/You win/i.test(result)) {
+            winnerCount++;
+        } else if (/You lose/i.test(result)) {
+            loserCount++;
+        }
+        console.log(result);
+    }
+    if (winnerCount > loserCount) {
+        return `You win with ${winnerCount} victories`;
+    } else if (winnerCount < loserCount) {
+        return `Computer win with ${loserCount} victories`
+    } else return `Tie! You and computer have ${winnerCount} victories`
+}
+
+console.log(game(evaluateTurn, getPlayerTurn, computerPlay));
